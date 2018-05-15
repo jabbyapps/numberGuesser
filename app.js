b9dc807e3e5f860bee95d2ad5e64249726f0bebd
jabbyapps <gbarnett@jabbyapps.com>
2018-05-14 20:51:39 -0400

    Final Commit - Project done.

--- app.js
+++ app.js
@@ -9,25 +9,25 @@ GAME FUNCTION:
 
 // Game values
 let min = 1,
-    max = 10,
-    winningNum = 2,
-    guessesLeft = 3;
+  max = 10,
+  winningNum = getRandomNum(min, max),
+  guessesLeft = 3;
 
 // UI Elements
 const ui_game = document.querySelector("#game"),
-      ui_minNum = document.querySelector(".min-num"),
-      ui_maxNum = document.querySelector(".max-num"),
-      ui_guessBtn = document.querySelector("#guess-btn"),
-      ui_guessInput = document.querySelector("#guess-input"),
-      ui_message = document.querySelector(".message");
+  ui_minNum = document.querySelector(".min-num"),
+  ui_maxNum = document.querySelector(".max-num"),
+  ui_guessBtn = document.querySelector("#guess-btn"),
+  ui_guessInput = document.querySelector("#guess-input"),
+  ui_message = document.querySelector(".message");
 
 // Assign UI min and max
 ui_minNum.textContent = min;
 ui_maxNum.textContent = max;
 
 // Play again event listener
-ui_game.addEventListener('mousedown', function(e){
-  if(e.target.className === 'play-again'){
+ui_game.addEventListener("mousedown", function(e) {
+  if (e.target.className === "play-again") {
     window.location.reload();
   }
 });
@@ -35,26 +35,26 @@ ui_game.addEventListener('mousedown', function(e){
 // Listen for guess
 ui_guessBtn.addEventListener("click", function() {
   let guess = parseInt(ui_guessInput.value);
-  
 
   // Validate
-  if(isNaN(guess) || guess < min || guess > max){
-    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red");
   }
 
   // Check if won
-  if(guess === winningNum){
+  if (guess === winningNum) {
     // Game over won.
     gameOver(true, `${winningNum} is correct, YOU WIN!!`);
-
   } else {
     // Wrong number
     guessesLeft -= 1;
 
-    if(guessesLeft === 0) {
+    if (guessesLeft === 0) {
       // Game over - lost
-      gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
-     
+      gameOver(
+        false,
+        `Game Over, you lost. The correct number was ${winningNum}`
+      );
     } else {
       // Game continues - answer wrong
 
@@ -62,18 +62,20 @@ ui_guessBtn.addEventListener("click", function() {
       ui_guessInput.style.borderColor = "red";
 
       // Clear input
-      ui_guessInput.value = '';
+      ui_guessInput.value = "";
 
       // Tell user it's the wrong number.
-      setMessage(`${guess} is not correct. You have ${guessesLeft} guesses left.`, "red");
-
+      setMessage(
+        `${guess} is not correct. You have ${guessesLeft} guesses left.`,
+        "red"
+      );
     }
   }
 });
 
-function gameOver(won,msg){
+function gameOver(won, msg) {
   let color;
-  won === true ? color = "green" : color = "red";
+  won === true ? (color = "green") : (color = "red");
 
   // Disable input
   ui_guessInput.disabled = true;
@@ -85,15 +87,16 @@ function gameOver(won,msg){
   setMessage(msg);
 
   // Play again?
-  ui_guessBtn.value = 'Play Again';
-  ui_guessBtn.className += 'play-again'
+  ui_guessBtn.value = "Play Again";
+  ui_guessBtn.className += "play-again";
+}
+// Get winning number
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Set message
-function setMessage(msg, color){
+function setMessage(msg, color) {
   ui_message.style.color = color;
   ui_message.textContent = msg;
 }
-
-
-
